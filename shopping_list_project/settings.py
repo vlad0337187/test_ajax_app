3234245cc8eed9272536c1a4fdfbf6002d3152d0
Vlad Naumov <vlad@develops.today>
2018-04-02 20:40:24 +0300

    Added css to login, register page; added info to TODO.txt; added sqlite3 database to test launch on system, where Postgres is absent.

--- shopping_list_project/settings.py
+++ shopping_list_project/settings.py
@@ -78,6 +78,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'shopping_list_project.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
